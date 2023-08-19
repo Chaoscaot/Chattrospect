da88441e8f7750376017df1e5a370785c2da5986
Chaoscaot <chaoscaot@zohomail.eu>
2023-08-20 00:05:47 +0200

    Fixing

--- src/main/java/de/chaoscaot/chattrospect/mixin/ChattrospectScreenMixin.java
+++ src/main/java/de/chaoscaot/chattrospect/mixin/ChattrospectScreenMixin.java
@@ -1,18 +1,13 @@
 package de.chaoscaot.chattrospect.mixin;
 
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.*;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(Screen.class)
+@Mixin(DrawContext.class)
 public class ChattrospectScreenMixin {
-
-    @Shadow @Final private static Logger LOGGER;
 
     private static final String[] MESSAGES = new String[] {
             "§9Open URL: §l%s",
@@ -23,7 +18,7 @@ public class ChattrospectScreenMixin {
             "§9Copy to Clipboard: §l%s"
     };
 
-    @Redirect(method = "renderTextHoverEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Style;getHoverEvent()Lnet/minecraft/text/HoverEvent;"))
+    @Redirect(method = "drawHoverEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Style;getHoverEvent()Lnet/minecraft/text/HoverEvent;"))
     private HoverEvent hoverEventGetHoverEventRedirect(Style instance) {
         HoverEvent hoverEvent = instance.getHoverEvent();
         boolean n = false;
